7d4fbd745d42dcb49d61bdd3f99ff4752d2fd3fc
Waleed Bilal <malik.waleed95@gmail.com>
2023-03-12 16:35:10 +0530

    cut down

---
+++ extras/navbarcode.php
@@ -0,0 +1,34 @@
+<td width="343" style="background-color:#468EFF">
+  <center>
+    <font size="+2"><b style="color:#191B7E">
+        <div style="background-color:#969BFB">College Updates</div><br>
+      </b></font>
+  </center>
+  <marquee direction="up" height="100%" onMouseOver="stop()" onMouseOut="start()">
+    <center><a href="index.php?option=cdrive" style="text-decoration:none">
+        <font size="+1"><b>Campus Drive</b></font>
+      </a></center><br>
+    <center><a href="index.php?option=news" style="text-decoration:none">
+        <font size="+1"><b>News</b></font>
+      </a></center><br>
+    <center><a href="index.php?option=sports" style="text-decoration:none">
+        <font size="+1"><b>Sports Fest</b></font>
+      </a></center><br>
+    <!--<center><a href="" style="text-decoration:none"><font size="+1"><b>Quiz Competition</b></font></a></center><br>-->
+    <center><a href="index.php?option=admission" style="text-decoration:none">
+        <font size="+1"><b>Admission 2014</b></font>
+      </a></center><br>
+    <center><a href="index.php?option=sdp" style="text-decoration:none">
+        <font size="+1"><b>Student Development Programme</b></font>
+      </a></center><br>
+    <center><a href="index.php?option=wevents" style="text-decoration:none">
+        <font size="+1"><b>Weekend Events</b></font>
+      </a></center><br>
+    <center><a href="index.php?option=notice" style="text-decoration:none">
+        <font size="+1"><b>Notice Board</b></font>
+      </a></center><br>
+    <!--<center><a href="" style="text-decoration:none"><font size="+1"><b>Summer Vacation</b></font></a></center><br>-->
+    <!--<center><a href="index.php?option=course" style="text-decoration:none"><font size="+1"><b>Courses Offered</b></font></a></center><br>
+            <center><a href="index.php?option=gallery" style="text-decoration:none"><font size="+1"><b>Gallery</b></font></a></center><br>-->
+  </marquee>
+</td>
--- wevents.php
+++
@@ -1,7 +0,0 @@
-<center><b>Weekend events:<b><br><br>
-
-Debate competiton(E/H)<br>
-
-Poetry inter college competiton<br>
-
-Basket ball competition<br></center>
